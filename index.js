fc457066c5fa3b2243610ef84ff7038e666fd30b
Yingqi Chen <chanwk18@hotmail.com>
2020-01-05 07:42:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@
    let timeInObj = this.timeInEvents.find(x=>x.date==date)
    let timeOutObj = this.timeOutEvents.find(x=>x.date==date)
 
-  return timeInObj.hour - timeOutObj.hour
+  return (timeInObj.hour - timeOutObj.hour)/100
 
 
  }
